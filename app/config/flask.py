3f8235d69b9853e7296642f564539b4775550824
14D00944 <14D00944@skvdiweb.com>
2021-01-27 17:32:16 +0900

    database settings

--- app/config/flask.py
+++ app/config/flask.py
@@ -5,11 +5,12 @@ class BaseConfig(object):
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(BaseConfig):
-  SQLALCHEMY_DATABASE_URI="mysql+mysqldb://dochi:dochi@127.0.0.1/scott?charset=utf8mb4"
+  pass
 
 class DevelopmentConfig(BaseConfig):
-  # SQLALCHEMY_DATABASE_URI="mysql+mysqldb://dochi:dochi@127.0.0.1/scott?charset=utf8mb4"
-  SQLALCHEMY_DATABASE_URI="oracle://LOTTO:lotto12!#@127.0.0.1:1521/CAMPDB"
+  # SQLALCHEMY_SQLITE_URI="sqlite:///app/database/example.db"
+  # SQLALCHEMY_MARIADB_URI="mysql+mysqldb://dochi:dochi@127.0.0.1/scott?charset=utf8mb4"
+  # SQLALCHEMY_ORACLE_URI="oracle://LOTTO:lotto12!#@127.0.0.1:1521/CAMPDB"
 
   DEV_SERVER_HOST = "0.0.0.0"
   DEV_SERVER_PORT = 3000
